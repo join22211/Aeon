cb8c9c01784aa2c24b267fc4a46a07e2795117cf
join22211 <join22211@gmail.com>
2024-08-03 16:36:32 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -202,7 +202,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = '<b>Powered by TOM BOT</b>\n\n'
+    msg = '<blockquote><b>Powered by TOM BOT</b></blockquote>\n\n'
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
